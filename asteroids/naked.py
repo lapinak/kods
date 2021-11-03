dcf0d7a8ad08501ecd9282e01cef6e2413b493fc
kal <karii.a.lap123@gmail.com>
2021-11-03 11:45:32 +0000

    Izmainīts API

--- asteroids/naked.py
+++ asteroids/naked.py
@@ -10,7 +10,7 @@ print('Asteroid processing service')
 # Initiating and reading config values
 print('Loading configuration from file')
 
-nasa_api_key = "???"
+nasa_api_key = "tpTJMfWQc4JOtXZbpWotfhklh9oTlTirhRnDa1gX"
 nasa_api_url = "https://api.nasa.gov/neo/"
 
 # Getting todays date
